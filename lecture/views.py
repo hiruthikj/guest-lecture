bb0f504fcd9859228080eb530a94653f2c0b3ef1
hiruthikj <hiruthik27@gmail.com>
2021-03-11 21:02:24 +0530

    feat: add events page

--- lecture/views.py
+++ lecture/views.py
@@ -1,17 +1,19 @@
+from django.db import models
 from django.shortcuts import reverse, render
+
 # from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 
 from datetime import datetime
-from .models import Event,applications
-
+from .models import Event, applications
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,15 +21,17 @@ class HomeView(LoginRequiredMixin, TemplateView):
         # context['book_list'] = Book.objects.all()
         return context
 
-def appliedEvents(request,user_pk):
+
+def appliedEvents(request, user_pk):
     apps = applications.objects.filter(student=user_pk)
     eventobj_list = []
     for app in apps:
         eventobj = Event.objects.get(event_name=app.event)
         eventobj_list.append(eventobj)
-    return render(request,'appliedEvents.html',{'events':eventobj_list})
+    return render(request, "appliedEvents.html", {"events": eventobj_list})
 
-def upcomingEvents(request,user_pk):
+
+def upcomingEvents(request, user_pk):
     eventobj_list = Event.objects.filter(status=Event.EventStatus.ON_SCHEDULE)
     eventobj_list = list(eventobj_list)
     apps = applications.objects.filter(student=user_pk)
@@ -39,7 +43,8 @@ def upcomingEvents(request,user_pk):
     for eventobj in eventobj_lists:
         eventobj_list.remove(eventobj)
     print(eventobj_list)
-    return render(request,'upcomingEvents.html',{'events':eventobj_list})
+    return render(request, "upcomingEvents.html", {"events": eventobj_list})
+
 
 def pastEvents(request, user_pk):
     apps = applications.objects.filter(student=user_pk)
@@ -48,7 +53,13 @@ def pastEvents(request, user_pk):
         eventobj = Event.objects.get(event_name=app.event)
         if eventobj.end_date > datetime.now():
             eventobj_list.append(eventobj)
-    return render(request,'pastEvents.html',{'events':eventobj_list})
+    return render(request, "pastEvents.html", {"events": eventobj_list})
+
+
+class EventView(DetailView):
+    template_name = "event.html"
+    model = Event
+    context_object_name = "event"
 
 # @login_required()
 # def home_view(request, username):
@@ -59,5 +70,5 @@ def pastEvents(request, user_pk):
 #         'student' : student,
 #         'name': student.get_name(),
 #         'current_page': 'home',
-        
+
 #     })
