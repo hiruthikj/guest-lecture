f28e212b7758bdcb801a883185d0d3e0d29f7b4f
hiruthikj <hiruthik27@gmail.com>
2021-04-04 11:43:35 +0530

    merge

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,26 +1,22 @@
 # from django.test import TestCase
-
-
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PlayerFormTest(LiveServerTestCase):
+LOCAL_SITE_URL = 'http://127.0.0.1:8000/'
+
+class LoginFormTest(LiveServerTestCase):
 
   def test_login_form(self):
-    selenium = webdriver.Chrome()
+    driver = webdriver.Chrome()
+    driver.get(LOCAL_SITE_URL)
     
-    selenium.get('http://127.0.0.1:8000/')
-    
-    username = selenium.find_element_by_name('username')
-    password = selenium.find_element_by_name('password')
-
-    submit = selenium.find_element_by_name('submit')
+    username = driver.find_element_by_name('username')
+    password = driver.find_element_by_name('password')
+    submit = driver.find_element_by_name('submit')
 
     username.send_keys('test_username')
     password.send_keys('test_password')
-
     submit.send_keys(Keys.RETURN)
 
-    #check result; page source looks at entire html document
-    assert 'test_username' in selenium.page_source
+    assert 'test_username' in driver.page_source
