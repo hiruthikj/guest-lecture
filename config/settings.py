f6563257753224126e0813494a4d41b3fbbe06ea
hiruthikj <hiruthik27@gmail.com>
2021-06-08 12:20:31 +0530

    fix: EMAIL env variable

--- config/settings.py
+++ config/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', default="nomail")
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', default="nopassword")
