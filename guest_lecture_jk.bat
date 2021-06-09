64697b05957196ef6f5569ba11fc101873d51eae
hiruthikj <hiruthik27@gmail.com>
2021-06-09 18:06:33 +0530

    Add collect static

--- guest_lecture_jk.bat
+++ guest_lecture_jk.bat
@@ -1,6 +1,7 @@
 cd "D:\projects\Django\lecture-management"
 
 pipenv install
+pipenv run python manage.py collectstatic
 pipenv run python manage.py jenkins
 
 @REM pipenv install -r requirements.txt --skip-lock
