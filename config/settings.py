8af7106324c9309cae96f2a061cf9ada8bda0162
hiruthikj <hiruthik27@gmail.com>
2021-06-09 17:50:33 +0530

    remove jenkins jslint csslint

--- config/settings.py
+++ config/settings.py
@@ -175,7 +175,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
-    'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_sloccount'
+    # 'django_jenkins.tasks.run_jslint',
+    # 'django_jenkins.tasks.run_csslint',
+    # 'django_jenkins.tasks.run_sloccount'
 )
