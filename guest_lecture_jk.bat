7c5121b997be5212472c276b026c6700f0abfb37
hiruthikj <hiruthik27@gmail.com>
2021-06-09 22:00:38 +0530

    testing webhook 0

--- guest_lecture_jk.bat
+++ guest_lecture_jk.bat
@@ -4,6 +4,12 @@ pipenv install
 pipenv run python manage.py collectstatic
 pipenv run python manage.py jenkins
 
+@REM git remote add heroku https://git.heroku.com/lecture-management.git
+@REM git push heroku master -f
+@REM heroku ps:scale web=1
+@REM heroku run python manage.py migrate
+
+
 @REM pipenv install -r requirements.txt --skip-lock
 @REM pipenv shell
 @REM pipenv run python manage.py jenkins --enable-coverage
