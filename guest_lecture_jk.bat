2958221e30ffc8f7df9b22e786e1e65d4f45735a
hiruthikj <hiruthik27@gmail.com>
2021-06-09 17:48:07 +0530

    install pyflakes

--- guest_lecture_jk.bat
+++ guest_lecture_jk.bat
@@ -1,6 +1,8 @@
 cd "D:\projects\Django\lecture-management"
 
 pipenv install
+pipenv run python manage.py jenkins
+
 @REM pipenv install -r requirements.txt --skip-lock
 @REM pipenv shell
-pipenv run python manage.py jenkins --enable-coverage
+@REM pipenv run python manage.py jenkins --enable-coverage
