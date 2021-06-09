6184f54156ff1e93e28e21e396c2a5bdf9bf476f
hiruthikj <hiruthik27@gmail.com>
2021-06-09 17:40:55 +0530

    install pep8

--- guest_lecture_jk.bat
+++ guest_lecture_jk.bat
@@ -1,6 +1,6 @@
 cd "D:\projects\Django\lecture-management"
 
-@REM pipenv install
-pipenv install -r requirements.txt --skip-lock
+pipenv install
+@REM pipenv install -r requirements.txt --skip-lock
 @REM pipenv shell
 pipenv run python manage.py jenkins --enable-coverage
